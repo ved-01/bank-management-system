dbba9f591dd4cce1d040a00f5b6f28a0227192f6
Ved Vekhande <vedvekhande7274@gmail.com>
2023-04-25 03:51:41 +0530

    added password in open account

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,9 +15,37 @@ const pool=mysql.createPool({
 exports.view = (req, res) => {
     res.render('home',{ layout: 'main' });
 }
+exports.loginpage = (req, res) => {
+    res.render('login',{ layout: 'loginlayout' });
+}
+
+exports.cardspage = (req, res) => {
+    res.render('cards',{ layout: 'cardslayout' });
+}
+exports.dashboardpage = (req, res) => {
+    res.render('dashboard',{ layout: 'dashboardlayout' });
+}
+exports.loanspage = (req, res) => {
+    res.render('loans',{ layout: 'loanslayout' });
+}
+exports.profilepage = (req, res) => {
+    res.render('profile',{ layout: 'profilelayout' });
+}
+exports.subscriptionspage = (req, res) => {
+    res.render('subscriptions',{ layout: 'subscriptionslayout' });
+}
+exports.supportpage = (req, res) => {
+    res.render('support',{ layout: 'supportlayout' });
+}
+exports.transactionspage = (req, res) => {
+    res.render('transactions',{ layout: 'transactionslayout' });
+}
+exports.adminpage = (req, res) => {
+    res.render('admin',{ layout: 'adminlayout' });
+}
 
 exports.create = (req,res) => {
-    const { name, username, contact_no} = req.body;
+    const { name, username, contact_no, password} = req.body;
     
         pool.getConnection((err, Connection) =>{
             if(err) throw err; //not connected
@@ -25,7 +53,7 @@ exports.create = (req,res) => {
     
             let searchTerm = req.body.search; //search is the actual input from user
             
-            Connection.query('INSERT INTO account_holder SET name = ?, username = ?, contact_no = ?',[ name, username, contact_no] ,(err, rows) =>{
+            Connection.query('INSERT INTO account_holder SET name = ?, username = ?, contact_no = ?, password = ?',[ name, username, contact_no, password] ,(err, rows) =>{
                 Connection.release();
     
                 if(!err){
@@ -57,33 +85,6 @@ exports.create = (req,res) => {
         });
       }
 
-    exports.loginpage = (req, res) => {
-        res.render('login',{ layout: 'loginlayout' });
-    }
-
-    exports.cardspage = (req, res) => {
-        res.render('cards',{ layout: 'cardslayout' });
-    }
-    exports.dashboardpage = (req, res) => {
-        res.render('dashboard',{ layout: 'dashboardlayout' });
-    }
-    exports.loanspage = (req, res) => {
-        res.render('loans',{ layout: 'loanslayout' });
-    }
-    exports.profilepage = (req, res) => {
-        res.render('profile',{ layout: 'profilelayout' });
-    }
-    exports.subscriptionspage = (req, res) => {
-        res.render('subscriptions',{ layout: 'subscriptionslayout' });
-    }
-    exports.supportpage = (req, res) => {
-        res.render('support',{ layout: 'supportlayout' });
-    }
-    exports.transactionspage = (req, res) => {
-        res.render('transactions',{ layout: 'transactionslayout' });
-    }
-    exports.adminpage = (req, res) => {
-        res.render('admin',{ layout: 'adminlayout' });
-    }
+    
 
     
